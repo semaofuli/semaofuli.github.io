eba0d5110e774254828273575700e0747dc2f209
semaofuli <135287351+semaofuli@users.noreply.github.com>
2024-09-07 22:32:09 +0800

    jquery.js

--- jquery.js
+++ jquery.js
@@ -18,9 +18,4 @@ window.onload=function(){
             }
 
         })()
-————————————————
-
-                            版权声明：本文为博主原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接和本声明。
-                        
-原文链接：https://blog.csdn.net/qq_42825887/article/details/129267877
-    }
+}

